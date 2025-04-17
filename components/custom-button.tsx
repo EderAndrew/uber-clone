c0f33f233b031b664e2403b03f2ab8f6ad8288d5
Andrew <ederandrew0028@gmail.com>
2025-04-16 23:56:04 -0300

    welcome updated

---
+++ components/custom-button.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { TouchableOpacity } from "react-native";
+
+export const CustomButton = ({ 
+    onPress,
+    title,
+    bgVariant="primary",
+    textVariant="default",
+    IconLeft,
+    IconRight,
+    className
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+        
+    </TouchableOpacity>
+  )
+};
