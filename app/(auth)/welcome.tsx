818ab164dde15d85d031fb1e633f8de722f2ae4a
Eder Andrew <eder.silva@montreal.com.br>
2025-04-17 09:26:14 -0300

    signUp created

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -5,10 +5,13 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 import { onboarding } from "../../constants";
+import { CustomButton } from "@/components/CustomButton";
 
 export default function Onboarding() {
     const swiperRef = useRef<Swiper>(null);
     const [activeIndex, setActiveIndex] = useState(0);
+    const isLastSlide = activeIndex === onboarding.length - 1;
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -38,6 +41,11 @@ export default function Onboarding() {
                 </View>
             ))}
         </Swiper>
+        <CustomButton
+          title={isLastSlide ? "Get Started" : "Next"}
+          onPress={() => isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current?.scrollBy(1)}
+          className="w-11/12 mt-10"
+        />
     </SafeAreaView>
   );
 }
