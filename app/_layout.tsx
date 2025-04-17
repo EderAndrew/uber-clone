1f7b51a04fa55e2909acbba537968d0675b7864e
Andrew <ederandrew0028@gmail.com>
2025-04-16 21:48:24 -0300

    tailwind configurated

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout(){
+    const [loaded] = useFonts({
+        "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+        "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+        "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+        "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    });
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { registerRootComponent } from 'expo';
+/* eslint-disable prettier/prettier */
+import { registerRootComponent } from "expo";
 import "./global.css"
-import App from './app/App';
+import App from './app/index';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
